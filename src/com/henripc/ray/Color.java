b5a2634e2b86738c742a5738ed549f6815b786c6
henripc <henriquechimetta@outlook.com>
2022-06-18 14:33:50 -0300

    Updated the Color class.
    Color class was updated to make use of the abstract Vector class.

--- src/com/henripc/ray/Color.java
+++ src/com/henripc/ray/Color.java
@@ -1,15 +1,15 @@
 package com.henripc.ray;
 
-public class Color extends Vec3 {
+public class Color extends Vector {
     public Color(final double e0, final double e1, final double e2) {
         super(e0, e1, e2);
     }
 
-    public static void writeColor(Color pixelColor) {
+    public static void writeColor(Vector pixelColor) {
         // Write the translated [0,255] value of each color component.
-        final String result = (int) (255.999 * pixelColor.x()) + " " +
-                              (int) (255.999 * pixelColor.x()) + " " +
-                              (int) (255.999 * pixelColor.x());
+        final String result = ((int) (255.999 * pixelColor.x())) + " " +
+                              ((int) (255.999 * pixelColor.y())) + " " +
+                              ((int) (255.999 * pixelColor.z()));
         System.out.println(result);
     }
 }
