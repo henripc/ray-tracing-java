aeb739344681a92e24c43f78124fbfb230880e11
henripc <henriquechimetta@outlook.com>
2022-06-19 12:25:24 -0300

    Added a Sphere class.
    A class that represents a hittable sphere.

---
+++ src/com/henripc/ray/Sphere.java
@@ -0,0 +1,37 @@
+package com.henripc.ray;
+
+public class Sphere implements Hittable {
+    public Point3 center;
+    public double radius;
+
+    public Sphere() {}
+    public Sphere(final Point3 cen, final double r) {
+        this.center = cen;
+        this.radius = r;
+    }
+
+    @Override
+    public boolean hit(final Ray r, final double tMin, final double tMax, final HitRecord rec) {
+        final Vector oc = Vector.sumOfVectors(r.getOrigin(), this.center.scalarMultiplication(-1));
+        final double a = r.getDirection().lengthSquared();
+        final double halfB = Vector.dot(oc, r.getDirection());
+        final double c = oc.lengthSquared() - this.radius * this.radius;
+
+        final double discriminant = halfB * halfB - a * c;
+        if (discriminant < 0) return false;
+        final double sqrtD = Math.sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        double root = (-halfB - sqrtD) / a;
+        if (root < tMin || tMax < root) {
+            root = (-halfB + sqrtD) / a;
+            if (root < tMin || tMax < root) return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = Vector.sumOfVectors(rec.p, this.center.scalarMultiplication(-1)).scalarDivision(this.radius);
+
+        return true;
+    }    
+}
