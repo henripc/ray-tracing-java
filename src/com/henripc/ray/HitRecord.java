f695cf8a6d77afa919dd7bbea323457b98be5629
henripc <henriquechimetta@outlook.com>
2022-06-19 12:31:53 -0300

    Updated the HitRecord class.
    Added a method to set the normal vector direction.

--- src/com/henripc/ray/HitRecord.java
+++ src/com/henripc/ray/HitRecord.java
@@ -4,8 +4,14 @@ public class HitRecord {
     public Vector p;
     public Vector normal;
     public double t;
+    public boolean frontFace;
 
     public HitRecord() {
 
+    }
+
+    public void setFaceNormal(final Ray r, final Vector outwardNormal) {
+        this.frontFace = Vector.dot(r.getDirection(), outwardNormal) < 0;
+        this.normal = frontFace ? outwardNormal : outwardNormal.scalarMultiplication(-1);
     }
 }
