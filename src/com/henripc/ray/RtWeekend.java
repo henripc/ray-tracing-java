7a319c718e2160cfa22c0a209ccd63cad2c1cbb4
henripc <henriquechimetta@outlook.com>
2022-06-19 14:16:27 -0300

    Added RtWeekend class.
    Added a utility class with some constants and helper methods.

---
+++ src/com/henripc/ray/RtWeekend.java
@@ -0,0 +1,12 @@
+package com.henripc.ray;
+
+public abstract class RtWeekend {
+    // Constants
+    public final static double INFINITY = Double.POSITIVE_INFINITY;
+    public final static double PI =  Math.PI;
+
+    // Utility Functions
+    public static double degreesToRadians(final double degrees) {
+        return degrees * PI / 180;
+    }
+}
