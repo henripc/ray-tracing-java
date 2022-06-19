4d1b5af093ffc691ae08635c3743efdfc8afcd10
henripc <henriquechimetta@outlook.com>
2022-06-19 20:17:09 -0300

    Update HitRecord class.
    Replaced all fields with a container (Map). Updating is now possible (work around of java's passing by value).

--- src/com/henripc/ray/HitRecord.java
+++ src/com/henripc/ray/HitRecord.java
@@ -1,17 +1,26 @@
 package com.henripc.ray;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HitRecord {
-    public Vector p;
-    public Vector normal;
-    public double t;
-    public boolean frontFace;
+    // private Vector p;
+    // private Vector normal;
+    // private double t;
+    // private boolean frontFace;
+
+    public final Map<String, Object> fieldsMap = new HashMap<>();
 
     public HitRecord() {
-
+        fieldsMap.put("p", null);
+        fieldsMap.put("normal", null);
+        fieldsMap.put("t", null);
+        fieldsMap.put("frontFace", null);
     }
 
     public void setFaceNormal(final Ray r, final Vector outwardNormal) {
-        this.frontFace = Vector.dot(r.getDirection(), outwardNormal) < 0;
-        this.normal = frontFace ? outwardNormal : outwardNormal.scalarMultiplication(-1);
+        this.fieldsMap.put("frontFace", Vector.dot(r.getDirection(), outwardNormal) < 0);
+        final Vector normal = (boolean) this.fieldsMap.get("frontFace") ? outwardNormal : outwardNormal.scalarMultiplication(-1);
+        this.fieldsMap.put("normal", normal);
     }
 }
