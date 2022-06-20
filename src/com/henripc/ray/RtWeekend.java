5d3d3a763735ffcc3a9f5c9e88df3c10fb1ed3e1
henripc <henriquechimetta@outlook.com>
2022-06-20 13:02:14 -0300

    Update RtWeekend class
    Added static methods for generating random numbers and a method to clamp a number to a specific interval.

--- src/com/henripc/ray/RtWeekend.java
+++ src/com/henripc/ray/RtWeekend.java
@@ -9,4 +9,21 @@ public abstract class RtWeekend {
     public static double degreesToRadians(final double degrees) {
         return degrees * PI / 180;
     }
+
+    public static double randomDouble() {
+        // Returns a random real in [0,1).
+        return Math.random();
+    }
+
+    public static double randomDouble(final double min, final double max) {
+        // Returns a random real in [min,max).
+        return min + (max - min) * randomDouble();
+    }
+
+    public static double clamp(final double x, final double min, final double max) {
+        if (x < min) return min;
+        if (x > max) return max;
+        
+        return x;
+    }
 }
