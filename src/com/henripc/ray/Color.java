2d4edf1cc4ce97b163975ff635ed04171e26c665
henripc <henriquechimetta@outlook.com>
2022-06-20 13:00:32 -0300

    Update Color class.
    Added samples per pixel to the class (antialiasing).

--- src/com/henripc/ray/Color.java
+++ src/com/henripc/ray/Color.java
@@ -5,11 +5,21 @@ public class Color extends Vector {
         super(e0, e1, e2);
     }
 
-    public static void writeColor(Vector pixelColor) {
+    public static void writeColor(final Vector pixelColor, final int samplesPerPixel) {
+        double r = pixelColor.x();
+        double g = pixelColor.y();
+        double b = pixelColor.z();
+
+        // Divide the color by the number of samples.
+        final double scale = 1.0 / samplesPerPixel;
+        r *= scale;
+        g *= scale;
+        b *= scale;
+
         // Write the translated [0,255] value of each color component.
-        final String result = ((int) (255.999 * pixelColor.x())) + " " +
-                              ((int) (255.999 * pixelColor.y())) + " " +
-                              ((int) (255.999 * pixelColor.z()));
+        final String result = ((int) (256 * RtWeekend.clamp(r, 0, 0.999))) + " " +
+                              ((int) (256 * RtWeekend.clamp(g, 0, 0.999))) + " " +
+                              ((int) (256 * RtWeekend.clamp(b, 0, 0.999)));
         System.out.println(result);
     }
 }
