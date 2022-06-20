35a32e8614bf590d818d9e4d179a07d3ec2359ce
henripc <henriquechimetta@outlook.com>
2022-06-20 12:24:43 -0300

    Update HitRecord class.
    Returned the fields and removed the HashMap.

--- src/com/henripc/ray/HitRecord.java
+++ src/com/henripc/ray/HitRecord.java
@@ -1,26 +1,16 @@
 package com.henripc.ray;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class HitRecord {
-    // private Vector p;
-    // private Vector normal;
-    // private double t;
-    // private boolean frontFace;
-
-    public final Map<String, Object> fieldsMap = new HashMap<>();
+    public Vector p;
+    public Vector normal;
+    public double t;
+    public boolean frontFace;
 
     public HitRecord() {
-        fieldsMap.put("p", null);
-        fieldsMap.put("normal", null);
-        fieldsMap.put("t", null);
-        fieldsMap.put("frontFace", null);
     }
 
     public void setFaceNormal(final Ray r, final Vector outwardNormal) {
-        this.fieldsMap.put("frontFace", Vector.dot(r.getDirection(), outwardNormal) < 0);
-        final Vector normal = (boolean) this.fieldsMap.get("frontFace") ? outwardNormal : outwardNormal.scalarMultiplication(-1);
-        this.fieldsMap.put("normal", normal);
+        this.frontFace = Vector.dot(r.getDirection(), outwardNormal) < 0;
+        this.normal = this.frontFace ? outwardNormal : outwardNormal.scalarMultiplication(-1);
     }
 }
