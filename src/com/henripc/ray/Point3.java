b0c8b5beba471c3c66e507a7a67a544fb4e76e15
henripc <henriquechimetta@outlook.com>
2022-06-21 12:40:06 -0300

    Added a default constructor.

--- src/com/henripc/ray/Point3.java
+++ src/com/henripc/ray/Point3.java
@@ -1,6 +1,9 @@
 package com.henripc.ray;
 
 public class Point3 extends Vector {
+    public Point3() {
+        super();
+    }
     public Point3(final double e0, final double e1, final double e2) {
         super(e0, e1, e2);
     }
