9363b7d19224632e14d5b199fa658e0dd72c310f
henripc <henriquechimetta@outlook.com>
2022-06-22 12:08:43 -0300

    Update the Camera class.
    Changed the Camera class to add points to look at and from, angle of the view and distance.

--- src/com/henripc/ray/Camera.java
+++ src/com/henripc/ray/Camera.java
@@ -6,25 +6,30 @@ public class Camera {
     private final Vector horizontal;
     private final Vector vertical;
 
-    public Camera() {
-        final double aspectRatio = 16 / 9.0;
-        final double viewportHeight = 2;
+    public Camera(final Point3 lookFrom, final Point3 lookAt, final Vec3 vUp,
+                  final double vFov, final double aspectRatio) {    // vFov vertical field-of-view in degrees
+        final double theta = RtWeekend.degreesToRadians(vFov);
+        final double h = Math.tan(theta / 2);
+        final double viewportHeight = 2 * h;
         final double viewportWidth = aspectRatio * viewportHeight;
-        final double focalLength = 1;
+        
+        final Vector w = Vector.unitVector(Vector.sumOfVectors(lookFrom, lookAt.scalarMultiplication(-1)));
+        final Vector u = Vector.unitVector(Vector.cross(vUp, w));
+        final Vector v = Vector.cross(w, u);
 
-        this.origin = new Point3(0, 0, 0);
-        this.horizontal = new Vec3(viewportWidth, 0, 0);
-        this.vertical = new Vec3(0, viewportHeight, 0);
+        this.origin = lookFrom;
+        this.horizontal = u.scalarMultiplication(viewportWidth);
+        this.vertical = v.scalarMultiplication(viewportHeight);
         this.lowerLeftCorner = Vector.sumOfVectors(this.origin,
                                                    this.horizontal.scalarMultiplication(-1.0 / 2),
                                                    this.vertical.scalarMultiplication(-1.0 / 2),
-                                                   (new Vec3(0, 0, focalLength)).scalarMultiplication(-1));
+                                                   w.scalarMultiplication(-1));
     }
 
-    public Ray getRay(final double u, final double v) {
-        return new Ray(this.origin, (Vec3) Vector.sumOfVectors(this.lowerLeftCorner,
-                                                               this.horizontal.scalarMultiplication(u),
-                                                               this.vertical.scalarMultiplication(v),
-                                                               this.origin.scalarMultiplication(-1)));
+    public Ray getRay(final double s, final double t) {
+        return new Ray(this.origin, Vector.sumOfVectors(this.lowerLeftCorner,
+                                                        this.horizontal.scalarMultiplication(s),
+                                                        this.vertical.scalarMultiplication(t),
+                                                        this.origin.scalarMultiplication(-1)));
     }
 }
