2f8da809487688298946817eeaf6421553c32226
henripc <henriquechimetta@outlook.com>
2022-06-24 11:12:29 -0300

    Update the Ray class.
    Added the time field, to generate rays at random times.

--- src/com/henripc/ray/Ray.java
+++ src/com/henripc/ray/Ray.java
@@ -3,14 +3,21 @@ package com.henripc.ray;
 public class Ray {
     public Vector orig;
     public Vector dir;
+    public double tm;
 
     public Ray() {
         this.orig = new Point3();
         this.dir = new Vec3();
     }
+    public Ray(final Vector origin, final Vector direction, final double time) {
+        this.orig = origin;
+        this.dir = direction;
+        this.tm = time;
+    }       
     public Ray(final Vector origin, final Vector direction) {
         this.orig = origin;
         this.dir = direction;
+        this.tm = 0;
     }
 
     public Vector getOrigin() {
@@ -19,6 +26,10 @@ public class Ray {
 
     public Vector getDirection() {
         return this.dir;
+    }
+
+    public double getTime() {
+        return this.tm;
     }
 
     public Vector at(final double t) {
