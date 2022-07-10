ba41a68ea4d6f16d8c8835e06178656683f523e1
henripc <henriquechimetta@outlook.com>
2022-07-10 11:15:46 -0300

    Update Hittable interface.
    Added boundingBox method that computes boxes for all the Hittables.

--- src/com/henripc/ray/Hittable.java
+++ src/com/henripc/ray/Hittable.java
@@ -2,4 +2,5 @@ package com.henripc.ray;
 
 public interface Hittable {
     public boolean hit(final Ray r, double tMin, double tMax, final HitRecord rec);
+    public boolean boundingBox(final double time0, final double time1, final AABB outputBox);
 }
